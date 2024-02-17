554e0d11ad18901250a006af181b31aac969ea92
Saad's grimoire of sorceries <114657050+Applethal@users.noreply.github.com>
2024-02-17 13:44:27 +0100

    fixed some css

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -8,27 +8,27 @@ function darkmode() {
     navigationElement.classList.toggle('dark-mode');
 
     if (bodyElement.classList.contains('dark-mode')) {
-        bodyElement.style.backgroundColor = '#000000'; // Completely dark
+        bodyElement.style.backgroundColor = '#000000';
         bodyElement.style.color = 'aliceblue';
 
-        navigationElement.style.backgroundColor = '#4E2C2A'; // A bit darker than original
+        navigationElement.style.backgroundColor = '#4E2C2A';
         navigationElement.style.color = 'aliceblue';
 
-        mainElement.style.backgroundColor = '#444444'; // Dark grey
+        mainElement.style.backgroundColor = '#444444';
         mainElement.style.color = 'aliceblue';
 
-        topElement.style.backgroundColor = '#444444'; // Dark grey
+        topElement.style.backgroundColor = '#444444';
         topElement.style.color = 'rgb(255, 255, 255)';
     } else {
         bodyElement.style.backgroundColor = '#B8252F';
         bodyElement.style.color = '';
-        navigationElement.style.backgroundColor = '#5C3A37'; // Original color
+        navigationElement.style.backgroundColor = '#5C3A37';
         navigationElement.style.color = '';
 
-        mainElement.style.backgroundColor = '#332929'; // Original color
+        mainElement.style.backgroundColor = '#332929';
         mainElement.style.color = 'aliceblue';
 
-        topElement.style.backgroundColor = '#332929'; // Original color
+        topElement.style.backgroundColor = '#332929';
         topElement.style.color = 'rgb(255, 255, 255)';
     }
 }
